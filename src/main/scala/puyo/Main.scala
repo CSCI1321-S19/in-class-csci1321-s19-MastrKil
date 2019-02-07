8be4680c2bbcfe73297054956be520c235dfcc7d
Arjun Thakar <athakar@trinity.edu>
2019-02-07 11:12:36 -0600

    We worked on the timer and movement.

--- src/main/scala/puyo/Main.scala
+++ src/main/scala/puyo/Main.scala
@@ -1,9 +1,6 @@
 package puyo
 
 import scalafx.application.JFXApp
-import scalafx.scene.Scene
-import scalafx.scene.canvas.Canvas
-import scalafx.animation.AnimationTimer
 
 object Main extends JFXApp {
   val canvasWidth = 6 * Renderer.CellSize
@@ -15,6 +12,26 @@ object Main extends JFXApp {
       val gc = canvas.graphicsContext2D
       val renderer = new Renderer(gc)
       val board = new Board
+
+      onKeyPressed = (ke: KeyEvent) => {
+        ke.code match {
+          case KeyCode.Up => board.upPressed()
+          case KeyCode.Down => board.downPressed()
+          case KeyCode.Left => board.leftPressed()
+          case KeyCode.Right => board.rightPressed()
+          case _ =>
+        }
+      }
+
+      onKeyReleased = (ke: KeyEvent) => {
+        ke.code match {
+          case KeyCode.Up => board.upPressed()
+          case KeyCode.Down => board.downPressed()
+          case KeyCode.Left => board.leftPressed()
+          case KeyCode.Right => board.rightPressed()
+          case _ =>
+        }
+      }
 
       var lastTime = -1L
       val timer = AnimationTimer(time => {
--- src/main/scala/puyo/Puyo.scala
+++ src/main/scala/puyo/Puyo.scala
@@ -2,8 +2,8 @@ package puyo
 
 class Puyo(val x: Int, val y: Int, val color: PuyoColor.Value) extends Boba {
 
-  override def fall(): Puyo = {
-    new Puyo(x, y+1, color)
+  override def mvoe(dx: Int, dy: Int): Puyo = {
+    new Puyo(x + dx, y + dy, color)
   }
 
 }
